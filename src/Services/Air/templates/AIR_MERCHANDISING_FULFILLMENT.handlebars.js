ab730a505c8c4f3f26f86cd393d89b7280c6ef6a
Dae Hee Kim <dhkim@sharevaluecorp.com>
2020-09-18 11:40:44 +0900

    update fulfillment template

--- src/Services/Air/templates/AIR_MERCHANDISING_FULFILLMENT.handlebars.js
+++ src/Services/Air/templates/AIR_MERCHANDISING_FULFILLMENT.handlebars.js
@@ -1,18 +1,24 @@
 module.exports = `
-<soapenv:Envelope
-  xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+<soap:Envelope
   xmlns:air="http://www.travelport.com/schema/air_v47_0"
   xmlns:univ="http://www.travelport.com/schema/universal_v47_0"
   xmlns:com="http://www.travelport.com/schema/common_v47_0"
-    <soapenv:Body>
+  xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+     <soap:Body>
         <univ:AirMerchandisingFulfillmentReq 
             AuthorizedBy="user" 
             TargetBranch="{{TargetBranch}}">
             <com:BillingPointOfSaleInfo OriginApplication="uAPI" />
-            <air:HostReservation Carrier="UA" CarrierLocatorCode="GX3B71" ProviderCode="1G" ProviderLocatorCode="8M587G" UniversalLocatorCode="0TJ1O7" />
+            <air:HostReservation
+                Carrier="{{platingCarrier}}"
+                CarrierLocatorCode="{{carrier_pnr}}"
+                ProviderCode="{{provider}}"
+                ProviderLocatorCode="{{pnr}}" 
+                UniversalLocatorCode="{{uapi_ur_locator}}" />
             <air:AirSolution>
                 {{#each passengers}}
-                <air:SearchTraveler Code="{{passengerTypeCode}}" BookingTravelerRef="{{uapi_traveler_ref}}" Key="{{@index}}">
+                <air:SearchTraveler Code="{{passengerTypeCode}}" BookingTravelerRef="{{{uapi_traveler_ref}}}" Key="{{@index}}"
+                                DOB="{{birthDate}}" Age="{{age}}">
                     <com:Name Prefix="{{prefix}}" First="{{firstName}}" Last="{{lastName}}" />
                 </air:SearchTraveler>
                 {{/each}}
@@ -26,11 +32,26 @@ module.exports = `
                     Origin="{{from}}" Destination="{{to}}"
                     DepartureTime="{{departure}}"
                     ClassOfService="{{bookingClass}}" />
-                <air:FareBasis Code="{{fareBasis}}" />
+                {{/each}}
+                {{#each fareBasisCodes}}
+                <air:FareBasis Code="{{this}}" />
                 {{/each}}
             </air:AirSolution>
-            <air:SpecificSeatAssignment BookingTravelerRef="{{uapi_traveler_ref}}" SegmentRef="{{uapi_segment_ref}}" SeatId="{{seat_id}}" />
+            {{#if optionalServices}}
+            <air:OptionalServices>
+                {{#each optionalServices}}
+                <air:OptionalService Type="{{type}}" Source="{{source}}" SupplierCode="{{supplierCode}}"
+                                 Quantity="{{quantity}}" ServiceSubCode="{{serviceSubCode}}" 
+                                 TotalPrice="{{totalPrice}}" BasePrice="{{basePrice}}">
+                    <com:ServiceData Data="{{seat_id}}" AirSegmentRef="{{{uapi_segment_ref}}}" BookingTravelerRef="{{{uapi_traveler_ref}}}" /> 
+                </air:OptionalService>
+                {{/each}}
+            </air:OptionalServices>
+            {{/if}}
+            {{#each seats}}
+                <air:SpecificSeatAssignment BookingTravelerRef="{{uapi_traveler_ref}}" SegmentRef="{{uapi_segment_ref}}" SeatId="{{seat_id}}" />
+            {{/each}}
         </univ:AirMerchandisingFulfillmentReq>
-    </soapenv:Body>
-</soapenv:Envelope>
+    </soap:Body>
+</soap:Envelope>
 `;
